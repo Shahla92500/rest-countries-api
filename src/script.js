b1acd88d1da40ace86e4983facb6fe081d66a5c3
Shahla <roksan78@yahoo.com>
2025-10-16 00:43:29 -0700

    1st page layout

--- src/script.js
+++ src/script.js
@@ -1,11 +1,12 @@
+const BASE_URL = 'https://restcountries.com'
 const countriesDiv = document.getElementById("countries");
 const cardTemplate = document.getElementById('card-template')
 
 document.addEventListener("DOMContentLoaded", async () => {
+  try {
   const res = await fetch(
-    "https://restcountries.com/v3.1/all?fields=name,flags,population,region,capital",
+    `${BASE_URL}/v3.1/all?fields=name,flags,population,region,capital`,    
   );
-
   if (!res.ok) {
     throw new Error("Error fetching data");
   }
@@ -16,12 +17,28 @@ document.addEventListener("DOMContentLoaded", async () => {
   data.forEach(country => {
     const  cardTemplateClone = cardTemplate.cloneNode(true);
     // console.log(cardTemplate);
-    cardTemplate.style.display = 'block'
+    cardTemplateClone.style.display = 'block'
+    // cardClone.classList.add('country-card');
+    cardTemplateClone.querySelector("img").src = country.flags.png
+    cardTemplateClone.querySelector("h2").textContent = country.name.common;
 
-    cardTemplate.firstElementChild.src = country.flags.png;
-    cardTemplate.firstElementChild.nextElementSibling.textContent = country.name.common
-
+    const lis = cardTemplateClone.querySelectorAll("li");
+    if (lis.length >= 3) {
+      lis[0].textContent = `Population: ${Number(country.population).toLocaleString()}`;
+      lis[1].textContent = `Region : ${country.region?? '-'}`;
+      lis[2].textContent = `Capital: ${Array.isArray(country.capital) ? (country.capital[0] ?? '—') : (country.capital ?? '—')}`;
+      // cardTemplate.firstElementChild.src = country.flags.png;
+      // cardTemplate.firstElementChild.nextElementSibling.textContent = country.name.common
+    } else {
+        console.warn("Expected 3 <li> elements but found:", lis.length);
+    }
     countriesDiv.appendChild(cardTemplateClone)
     
   })
+    } catch (e) {
+    console.error(e);
+  }
 });
+
+
+
